1b1534251da3b08803d062feb96b062de83b7f25
ajh123 <ajh123@users.noreply.github.com>
2024-01-14 19:50:52 +0000

    fix: use environment variable for site

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,6 @@ import { dev } from '$app/environment';
 
 export const title = 'Miners Online';
 export const description = 'The official site for Miners Online';
-export const url = dev ? 'http://localhost:5173/' : 'https://minersonline.uk/';
+export const url = dev ? 'http://localhost:5173/' : (
+    process.env.SERVER_URL != undefined ? process.env.SERVER_URL : 'https://minersonline.uk/'
+);
