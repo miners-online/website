728d365301059d567e42aac3bdfb4b7dd273b053
ajh123 <ajh123@users.noreply.github.com>
2024-01-14 20:14:35 +0000

    fix: maybe fix environment variables

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,9 @@
 import { dev } from '$app/environment';
+// @ts-ignore TODO: fix typing
+import { PUBLIC_SERVER_URL } from '$env/static/public';
 
 export const title = 'Miners Online';
 export const description = 'The official site for Miners Online';
 export const url = dev ? 'http://localhost:5173/' : (
-    process.env.SERVER_URL != undefined ? process.env.SERVER_URL : 'https://minersonline.uk/'
+    PUBLIC_SERVER_URL != undefined ? PUBLIC_SERVER_URL : 'https://minersonline.uk/'
 );
