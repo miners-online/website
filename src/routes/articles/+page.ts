a70851e7bf058ba38690ff329316314e8a834eff
ajh123 <ajh123@users.noreply.github.com>
2024-01-14 18:22:40 +0000

    fix: fix all articles page

--- src/routes/articles/+page.ts
+++ src/routes/articles/+page.ts
@@ -1,6 +1,6 @@
 // @ts-ignore TODO: fix typing in Svelte
 export const load = async ({ fetch }) => {
-	const response = await fetch(`/api/articles`);
+	const response = await fetch(`/api/articles/json`);
 	const posts = (await response.json()).posts;
 
 	return {
