24b7446d565aeba5bf27f450fc46a99e970782ca
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2024-10-28 17:38:44 +0000

    fix: fix `react/no-unescaped-entities`

--- packages/docs/app/page.tsx
+++ packages/docs/app/page.tsx
@@ -7,7 +7,7 @@ const projects = [
   {
     id: 1,
     title: "Miners Online Wiki",
-    content: <p>A collaborative wiki for tutorials, FAQs, and documentation. <i>(this very website you're on!)</i></p>
+    content: <p>A collaborative wiki for tutorials, FAQs, and documentation. <i>(this very website you&rsquo;re on!)</i></p>
   },
   {
     id: 2,
@@ -45,13 +45,13 @@ export default function Home() {
       </div>
       <div className="flex flex-col px-2 pb-8">
         <Typography>
-          <h2>What's Miners Online</h2>
+          <h2>What&rsquo;s Miners Online</h2>
           <p>Miners Online is a collaborative, open-source platform that brings together enthusiasts to create, contribute, and innovate.</p>
           <p>Based in the United Kingdom, Miners Online is a place where community-driven development flourishes.</p>
 
           <h2>Platform Overview</h2>
           <p>Miners Online supports community-driven projects using modern frameworks to encourage creativity and innovation.</p>
-          <p>Whether you're an experienced developer or just starting, we welcome your contributions.</p>
+          <p>Whether you&rsquo;re an experienced developer or just starting, we welcome your contributions.</p>
 
           <h2>Projects</h2>
           <CardGrid cards={projects} columns={2} gap={4} />
