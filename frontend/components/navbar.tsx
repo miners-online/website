b07491fc93dbd04a55d8160ce18957a9563a1f57
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2025-03-24 18:11:06 +0000

    fix: update HeroSection component and replace img with Image component in Navbar

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -5,6 +5,7 @@ import Link from "next/link"
 import { Menu, X } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs"
+import Image from "next/image"
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
@@ -14,7 +15,7 @@ export default function Navbar() {
       <div className="container flex h-16 items-center px-4">
         <div className="flex items-center gap-2 mr-4">
           <div className="w-8 h-8 rounded flex items-center justify-center">
-            <img src="/favicon.svg"></img>
+            <Image width="64" height="64" alt="Miners Online Logo"  src="/favicon.svg"></Image>
           </div>
           <span className="font-bold text-xl">Miners Online</span>
         </div>
