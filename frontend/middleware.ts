f9c434c415ac42f15280e25ff948dded0fc8c565
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2025-06-05 16:42:07 +0100

    feat: add middleware for redirecting www to non-www host

---
+++ frontend/middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export const runtime = 'experimental-edge';
+
+export function middleware(request: NextRequest) {
+  const host = request.headers.get('host') || '';
+  
+  if (host.startsWith('www.')) {
+    const nonWwwHost = host.replace(/^www\./, '');
+    const url = request.nextUrl.clone();
+    url.host = nonWwwHost;
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
